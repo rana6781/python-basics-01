add670ffb7a20566981bdf0970d0c935143bb0dc
rana6781 <ranasaif6781@gmail.com>
2026-02-23 15:21:30 +0500

    basic.py01

--- bsics.py
+++ bsics.py
@@ -1,1 +1,31 @@
-print('Hello, World!')
+#comments
+
+#this code is replacing empty spaces in the text
+#variables understanding
+#
+price = 300
+salary = 5000.5
+name = "saif"
+items = ["apple", "banana", "orange"]
+static_items = ("u009","123456","4500")
+user_info = {'name':"saif",'age':23,"address":"abc multan pak"}
+base_values = {12,13,14,15,16,17}
+
+# function
+#20,30
+#100,400
+#50,950
+ 
+print(type(base_values))
+
+def sum(val1 ,val2):
+    return val1+val2
+
+value1 =20
+value2 =30
+sum1 =value1 + value2
+print("sum1 ans:",sum1)
+# adding sum1 values by using function sum
+print("adding sum1 values by using function sum", sum(30,30))
+
+      
