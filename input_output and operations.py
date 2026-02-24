2ab82afb06d2b5b7b6457a2c04e18d74f6636070
rana6781 <ranasaif6781@gmail.com>
2026-02-24 15:19:55 +0500

    input_output and operations.py

---
+++ input_output and operations.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 20
+add = num1 + num2
+res = add/2
+# print(num1)
+
+
+#vaiables
+#datatypes
+#functions: built functions print(),type()
+#add two values.
+
+
+num1 = 100
+num2 = 100
+add = num1 + num2
+res = add/2
+print(num1)
