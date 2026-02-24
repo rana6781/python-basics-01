37cde84c2c3c6260bd1a98e37ebf5d96e8f48017
rana6781 <ranasaif6781@gmail.com>
2026-02-24 15:35:25 +0500

    using input functions

--- input_output and operations.py
+++ input_output and operations.py
@@ -15,4 +15,17 @@ num1 = 100
 num2 = 100
 add = num1 + num2
 res = add/2
-print(num1)
+#print(num1)
+
+#input()
+# input()
+# input()
+
+#int(input("enter a number of your choice: "))
+#user_input =int( input("enter a number of your choice: "))
+#print("user input is:", user_input)
+
+num5 =int(input("enter first number to add" ))
+num6 =int(input("enter second number to add" ))
+add = num5 + num6   
+print("addition of two numbers is:", add)
