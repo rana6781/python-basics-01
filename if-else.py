dc9c480ee55c85ddb5c255ad862f53babdcbbd9b
rana6781 <ranasaif6781@gmail.com>
2026-03-06 14:44:23 +0500

    we are learning loops in phython

---
+++ if-else.py
@@ -0,0 +1,13 @@
+# age = float(input("Enter your age: "))
+# if age >= 18:
+#    print("welcome you're able to proceed your id card")
+
+
+marks = float(input("Enter your marks: "))
+
+if marks >=90:
+    print("Congratulations you're passed ")
+else:
+    print("Sorry you're failed,try again")
+
+    
---
+++ loop.py
@@ -0,0 +1,14 @@
+# list=[1,2,3,4,5,6,7]
+# for i in list :
+#     print(i)
+
+# items =["apple","banana","grapes","orange"]
+# for item in items:
+#     print(item)
+
+12
+3
+5
+9
+for character in "we are learning python":
+ print(character)
